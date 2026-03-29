dc90481d7efc6b3a2d5ee0c2a37706ab7245e84e
Ilknur <ilknurkazimov308@gmail.com>
2026-03-29 23:42:19 +0400

    Fix pycodestyle in task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -39,7 +39,8 @@ def matrix_divided(matrix, div):
         for elem in row:
             if not isinstance(elem, (int, float)):
                 raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats"
+                    "matrix must be a matrix (list of lists) of" 
+                    "integers/floats"
                 )
 
     new_matrix = []
