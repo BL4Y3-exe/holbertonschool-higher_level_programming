06a8e1fb3c71310de52dce8cd436fc9cb1407b04
Ilknur <ilknurkazimov308@gmail.com>
2026-03-29 23:06:40 +0400

    Add 0-add_integer.py func and its test cases, also added .gitignore

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a function that adds two integers
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integrs and returns the result.
+    
+    a and b must be integers or floats.
+    Floats are casted to integers before addition.
+    
+    Raises:
+        TypeError: if a or b is not an integer or float.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
