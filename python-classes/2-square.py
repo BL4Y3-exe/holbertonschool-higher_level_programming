845289bea831da8c371190a0e915f8bd53ccf1c3
Ilknur <ilknurkazimov308@gmail.com>
2026-04-02 14:55:51 +0400

    Fix pycodestyle in task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,16 +4,16 @@
 
 class Square:
     """Represents a square
-    
+
     Attributes:
         __size (int): The size of the square (private)
     """
     def __init__(self, size=0):
         """Initialize a Square
-        
+
         Args:
             size (int): The size of the square
-        
+
         Raises:
             TypeError: if size is not an integer
             ValueError: if size is less than 0
@@ -22,5 +22,5 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = size
