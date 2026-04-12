43e9ceb098eea762ee944d8d938f9071d4d558d1
Ilknur <ilknurkazimov308@gmail.com>
2026-04-12 20:05:56 +0400

    Add 8-rectangle.py

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module contains Rectangle class."""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Subclass of BaseGeometry.
+    
+    Args:
+        BaseGeometry: a parent class.
+        
+    Attributes:
+        __width: width of rectangle.
+        __height: height of rectangle.
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__height = height
+        self.__width = width
