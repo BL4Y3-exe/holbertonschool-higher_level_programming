06c1efd0f97b7bae7cc4492fb35ee52cff3bef29
Ilknur <ilknurkazimov308@gmail.com>
2026-04-19 20:07:57 +0400

    Add 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module contains script to add arguments to a Python list."""
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    ls = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    ls = []
+
+for i in range(1, len(sys.argv)):
+    ls.append(sys.argv[i])
+
+save_to_json_file(ls,"add_item.json")
