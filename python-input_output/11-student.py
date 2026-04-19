461ee538203a7fc9b6a81eb202df4abfb6e3e5e4
Ilknur <ilknurkazimov308@gmail.com>
2026-04-19 20:48:32 +0400

    Add 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""This module contains Student class."""
+
+
+class Student:
+    """
+    This class defines Student.
+
+    Attributes:
+        first_name: the name of student.
+        last_name: the last name of student.
+        age: the age of student.
+
+    Methods:
+        to_json: retrieves a dictionary representation of a Student instance.
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Function creates a dictionary of instance's attributes.
+
+        Retunrs:
+            dict: dictionary of the attributes.
+        """
+        dct = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
+
+        if (attrs is None):
+            return (dct)
+        new_dct = {}
+        for attr in attrs:
+            if attr in dct.keys():
+                new_dct[attr] = dct[attr]
+        return (new_dct)
+
+    def reload_from_json(self, json):
+        """
+        Function  replaces all attributes of the Student instance.
+
+        Args:
+            json: dictionary of attributes.
+        """
+        if json:
+            self.first_name = json["first_name"]
+            self.last_name = json["last_name"]
+            self.age = json["age"]
