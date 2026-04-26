d9e9b048277ac679ddb317f6aee054a86f891bd1
Ilknur <ilknurkazimov308@gmail.com>
2026-04-26 19:22:43 +0400

    Add task_03_http_server.py

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module contains MyHandler class and run function."""
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+
+def run(class_server=HTTPServer, class_handler=MyHandler):
+    server_adress = ('', 8000)
+    httpd = class_server(server_adress, class_handler)
+    httpd.serve_forever()
+
+
+run()
