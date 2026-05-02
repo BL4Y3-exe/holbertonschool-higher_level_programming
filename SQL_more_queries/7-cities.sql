4cab6b8f7d6f68993dd35aa42c84b95a96f6a771
Ilknur <ilknurkazimov308@gmail.com>
2026-05-02 21:58:46 +0400

    fix 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
--- script creates the database "hbtn_0d_usa" and the table "cities"
+-- Create the database "hbtn_0d_2" and table "cities"
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREING KEY (state_id) REFERENCES states(id)
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
