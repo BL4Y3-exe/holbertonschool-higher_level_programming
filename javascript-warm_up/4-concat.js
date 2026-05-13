936d08fb3109e35da38c2325c6ae187a53df42b1
Ilknur <ilknurkazimov308@gmail.com>
2026-05-13 14:36:11 +0400

    Fix 4-concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-  console.log('No arguments');
-} else {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
